2a2b681bb277c9b4c1e29811e1d45d0f38485df7
Anameskhi <ameskhi@unisens.ge>
2022-07-21 17:32:07 +0400

    completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @user.update(views: @user.views + 1)
+    @posts = Post.all.order(created_at: :desc)
+    if current_user.id != @user.id
+      @user.update(views: @user.views + 1)
+    end
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_084229) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_114116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,6 +121,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_084229) do
     t.string "provider"
     t.string "uid"
     t.string "avatar"
+    t.integer "views", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
