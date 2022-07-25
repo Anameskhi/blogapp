23c906bd7c2c6af6f18bf6632eef0cda7540825d
Anameskhi <ameskhi@unisens.ge>
2022-07-25 17:29:25 +0400

    ad pagy users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,16 +1,17 @@
 class AdminController < ApplicationController
   def index
+
   end
 
   def posts
-    @pagy,@posts = pagy(Post.all.includes(:user, :comments), items: 15)
+    @pagy,@posts = pagy(Post.all.includes(:user, :comments), items: 10)
   end
 
   def comments
   end
 
   def users
-    @users = User.all
+    @pagy,@users = pagy(User.all, items: 10)
   end
 
 
