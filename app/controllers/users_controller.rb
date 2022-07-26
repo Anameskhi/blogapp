63f4114142e45700abd258e35043c64feababfa7
Anameskhi <ameskhi@unisens.ge>
2022-07-26 16:40:37 +0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @pagy, @posts = pagy(@user.posts.order(created_at: :desc), items: 5)
-    @user.update(views: @user.views + 1) if current_user.id != @user.id
+
+    if current_user.nil? || ( !current_user.nil? &&  current_user.id != @user.id)
+    @user.update(views: @user.views + 1)
+    end
+    
+
+
+
   end
 end
