281006f8cfc9e2d5002251d2bf9685158aef03fa
Anameskhi <ameskhi@unisens.ge>
2022-08-24 16:16:04 +0400

    chngs

--- app/javascript/controllers/maps_controller.js
+++ app/javascript/controllers/maps_controller.js
@@ -1,36 +1,118 @@
-// import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
-// export default class extends Controller{
+export default class extends Controller{
+  static targets = ["field", "map", "latitude", "longitude"]
+
+  connect(){
+   if(typeof(google) != "undefinded"){
+     this.initializeMap();
+     console.log('initialize map');
+   }
+  }
+  initializeMap(){
+    console.log('map');
+    this.map()
+    this.marker()
+    this.autocomplete()
+  }
+  map(){
+    if(this._map == undefined){
+      this._map = new google.maps.Map(this.mapTarget,{
+        center: new google.maps.LatLng(
+          this.latitudeTarget.value,
+          this.longitudeTarget.value
+        ),
+        zoom: 17
+      })
+
+    }
+    return this._map
+  }
+  marker(){
+    console.log('marker');
+    if(this._marker == undefined){
+     this._marker = new google.maps.Marker({
+        map: this.map(),
+        anchorPoint: new google.maps.Point(0,0)
+      })
+      let mapLocation = {
+        lat: parseFloat(this.latitudeTarget.value),
+        lng: parseFloat(this.longitudeTarget.value)
+      }
+      this._marker.setPosition(mapLocation)
+      this._marker.setVisible(true)
+
+    }
+    return this._marker
+  }
+  autocomplete(){
+    if(this._autocomplete == undefined){
+      console.log('authocompalate');
+      this._autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
+      this._autocomplete.bindTo('bounds', this.map())
+      this._autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+      this._autocomplete.addListener('place_changed',this.locationChanged.bind(this))
+    }
+    return this._autocomplete
+  }
+
+  locationChanged(){
+    console.log('place')
+    let place = this.autocomplete().getPlace()
+    if(!place.geometry){
+      window.alert("No details available for input: '"+ place.name + "'");
+      return;
+    }
+    this.map().fitBounds(place.geometry.viewport)
+    this.map().setCenter(place.geometry.location)
+    this.marker().setPosition(place.geometry.location)
+    this.marker().setVisible(true)
+
+    this.latitudeTarget.value = place.geometry.location.lat()
+    this.longitudeTarget.value = place.geometry.location.lng()
+
+  }
+
+  preventSubmit(e){
+    if(e.key == "Enter"){e.preventDefault()}
+  }
+}
+
+
+// import { Controller} from "@hotwired/stimulus"
+
+// export default class extends Controller {
 //   static targets = ["field", "map", "latitude", "longitude"]
 
-//   connect(){
-//    if(typeof(google) != "undefinded"){
-//      this.initializeMap();
-//      console.log('initialize map');
-//    }
+//   connect() {
+//     if (typeof (google) != "undefined"){
+//       this.initializeMap()
+//     }
 //   }
-//   initializeMap(){
-//     console.log('map');
+
+//   initializeMap() {
 //     this.map()
 //     this.marker()
 //     this.autocomplete()
+//     console.log('init')
 //   }
-//   map(){
-//     if(this._map == undefined){
-//       this._map = new google.maps.Map(this.mapTarget,{
-//         center: new google.maps.LatLng(
-//           this.latitudeTarget.value,
-//           this.longitudeTarget.value
-//         ),
-//         zoom: 17
-//       })
 
+//   map() {
+//     if(this._map == undefined) {
+//       this._map = new google.maps.Map(this.mapTarget, {
+//         center: new google.maps.LatLng(
+//           // if latlng undefined, point it to georgia with zoom scale of 1
+//           this.latitudeTarget.value ? this.latitudeTarget.value : 42.315407,
+//           this.longitudeTarget.value ? this.longitudeTarget.value : 43.35689199999999
+//         ),
+//         zoom: this.latitudeTarget.value ? 17 : 1
+//       })
 //     }
 //     return this._map
 //   }
-//   marker(){
-//     console.log('marker');
-//     if(this._marker == undefined){
+
+//   marker() {
+//     if (this._marker == undefined) {
 //       this._marker = new google.maps.Marker({
 //         map: this.map(),
 //         anchorPoint: new google.maps.Point(0,0)
@@ -41,28 +123,30 @@
 //       }
 //       this._marker.setPosition(mapLocation)
 //       this._marker.setVisible(true)
-
 //     }
 //     return this._marker
 //   }
-//   autocomplete(){
-//     if(this._autocomplete == undefined){
-//       console.log('authocompalate');
+
+//   autocomplete() {
+//     if (this._autocomplete == undefined) {
 //       this._autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
 //       this._autocomplete.bindTo('bounds', this.map())
 //       this._autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
-//       this._autocomplete.addListener('place_changed',this.locationChanged.bind(this))
+//       this._autocomplete.addListener('place_changed', this.locationChanged.bind(this))
 //     }
 //     return this._autocomplete
 //   }
 
-//   locationChanged(){
-//     console.log('place')
+//   locationChanged() {
 //     let place = this.autocomplete().getPlace()
-//     if(!place.geometry){
-//       window.alert("No details available for input: '"+ place.name + "'");
+
+//     if (!place.geometry) {
+//       // User entered the name of a Place that was not suggested and
+//       // pressed the Enter key, or the Place Details request failed.
+//       window.alert("No details available for input: '" + place.name + "'");
 //       return;
 //     }
+
 //     this.map().fitBounds(place.geometry.viewport)
 //     this.map().setCenter(place.geometry.location)
 //     this.marker().setPosition(place.geometry.location)
@@ -70,94 +154,10 @@
 
 //     this.latitudeTarget.value = place.geometry.location.lat()
 //     this.longitudeTarget.value = place.geometry.location.lng()
-
 //   }
 
-//   preventSubmit(e){
-//     if(e.key == "Enter"){e.preventDefault()}
+//   preventSubmit(e) {
+//     if (e.key == "Enter") { e.preventDefault() }
 //   }
-// }
-
-
-import { Controller} from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["field", "map", "latitude", "longitude"]
-
-  connect() {
-    if (typeof (google) != "undefined"){
-      this.initializeMap()
-    }
-  }
-
-  initializeMap() {
-    this.map()
-    this.marker()
-    this.autocomplete()
-    console.log('init')
-  }
-
-  map() {
-    if(this._map == undefined) {
-      this._map = new google.maps.Map(this.mapTarget, {
-        center: new google.maps.LatLng(
-          // if latlng undefined, point it to georgia with zoom scale of 1
-          this.latitudeTarget.value ? this.latitudeTarget.value : 42.315407,
-          this.longitudeTarget.value ? this.longitudeTarget.value : 43.35689199999999
-        ),
-        zoom: this.latitudeTarget.value ? 17 : 1
-      })
-    }
-    return this._map
-  }
-
-  marker() {
-    if (this._marker == undefined) {
-      this._marker = new google.maps.Marker({
-        map: this.map(),
-        anchorPoint: new google.maps.Point(0,0)
-      })
-      let mapLocation = {
-        lat: parseFloat(this.latitudeTarget.value),
-        lng: parseFloat(this.longitudeTarget.value)
-      }
-      this._marker.setPosition(mapLocation)
-      this._marker.setVisible(true)
-    }
-    return this._marker
-  }
-
-  autocomplete() {
-    if (this._autocomplete == undefined) {
-      this._autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
-      this._autocomplete.bindTo('bounds', this.map())
-      this._autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
-      this._autocomplete.addListener('place_changed', this.locationChanged.bind(this))
-    }
-    return this._autocomplete
-  }
-
-  locationChanged() {
-    let place = this.autocomplete().getPlace()
-
-    if (!place.geometry) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
-
-    this.map().fitBounds(place.geometry.viewport)
-    this.map().setCenter(place.geometry.location)
-    this.marker().setPosition(place.geometry.location)
-    this.marker().setVisible(true)
-
-    this.latitudeTarget.value = place.geometry.location.lat()
-    this.longitudeTarget.value = place.geometry.location.lng()
-  }
-
-  preventSubmit(e) {
-    if (e.key == "Enter") { e.preventDefault() }
-  }
   
-}
+// } 
